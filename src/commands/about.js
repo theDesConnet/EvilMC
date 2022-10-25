aeb2104e9fbc1792b387415db07ae6b0538f94d7
лилия <82108363+LiliyaRU@users.noreply.github.com>
2022-10-26 00:58:42 +0700

    English Translation

--- src/commands/about.js
+++ src/commands/about.js
@@ -9,7 +9,7 @@ module.exports = new Command({
     permissions: "SEND_MESSAGES",
     slashCommandOptions: [],
     async execute(client, args, interaction){
-        let embed = new Discord.MessageEmbed().setThumbnail(client.user.displayAvatarURL(dynamic = true)).setTitle("О боте").setDescription("Бот для проверки серверов Minecraft на устойчивость к различный методам краша\n\n**c0d9d by DesConnet**").setFooter("Версия: 1.1")
+        let embed = new Discord.MessageEmbed().setThumbnail(client.user.displayAvatarURL(dynamic = true)).setTitle("About Bot").setDescription("A bot to test Minecraft servers stability with various crash methods\n\n**c0d9d by DesConnet**").setFooter("Version: 1.1")
         interaction.reply({ embeds: [embed] });
     }
 })
