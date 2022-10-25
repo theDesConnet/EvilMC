5a19bd3fd7feaf62b1780c3f4628c7caacc3efa2
лилия <82108363+LiliyaRU@users.noreply.github.com>
2022-10-26 01:04:04 +0700

    English Translation

--- src/commands/extremecrasher.js
+++ src/commands/extremecrasher.js
@@ -46,7 +46,7 @@ module.exports = new Command({
         const unstop = args.getBoolean("unstopable") || false;
 
 
-        if (hostValidattor(host) == false) return crashers.errorembed(client, interaction, interaction.commandName, "Не валидный IP"); 
+        if (hostValidattor(host) == false) return crashers.errorembed(client, interaction, interaction.commandName, "Invalid IP"); 
 
         const attack = new Attack({
             jaroptions: {
